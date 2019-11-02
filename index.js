6d4b124a4ffab0ea18046302911df3642f7ff857
meron <malemu17@yahoo.com>
2019-11-02 16:48:56 -0700

    someEdit

--- index.js
+++ index.js
@@ -13,7 +13,10 @@ const People = require('./models/people.js');
 
 let people = new People();
 let team = new Teams();
-
+/**
+ * which creates new people to our database
+ * @function makePerson
+ */
 const makePerson = async () => {
   let peopled = {
     firstName: 'James',
@@ -28,26 +31,34 @@ const makePerson = async () => {
   console.log('all Person', allPerson);
 
 }
-
+/**
+ * this makes new team to our data base
+ * @function maketeam
+ */
 const makeTeam = async () => {
   let team1 = {
     teamName: 'Purple Cat',
     color: 'purple',
-    memberName: ''
   }
-
   let newTeam = await team.create(team1);
   console.log('team created:', newTeam);
-
 }
-
+    
+/**
+ * This counts our team and people data length
+ * @function countData
+ */
 const countData = async () => {
   let Team = await team.count('_id');
   console.log('Teams', Team) ;
   let Person = await people.count('_id');
   console.log('People', Person)
 }
-
+/**
+ * this returns person detail information form our database
+ * if the firstName and lastName field is correct 
+ * @function personInformation
+ */
 const personInformation = async () => {
  let detail = await people.getByQuery(people.firstName, people.lastName);
  console.log(detail)
@@ -57,23 +68,29 @@ const personInformation = async () => {
     //({name:firstName, Team:people.teamName, Birthday:people.nextBirthdate, Likes:people.likes});
   else return "No record Found"
   
+  /**
+   * This function call each the above functions asynchronously 
+   * @function  foo 
+   * run each function asyncroniously 
+   */
+  
+  async function foo() {
+    await mongoose.connect(db, configs);
+    console.log('connected');
+    //await makePerson(); 
+    console.log('person created!');
+   //await makeTeam();
+   await countData();
+   await personInformation();
+   console.log('team created!');
+    await mongoose.connection.close();
+    console.log("closed"); 
+  }
+  
+  foo(); 
 }
 
 
-async function foo() {
-  await mongoose.connect(db, configs);
-  console.log('connected');
-  //await makePerson(); 
-  console.log('person created!');
- //await makeTeam();
- await countData();
- await personInformation();
- console.log('team created!');
-  await mongoose.connection.close();
-  console.log("closed"); 
-}
-
-foo(); 
     
     
 
